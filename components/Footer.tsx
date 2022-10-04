7827982e2e6dbd27c0462af571182c84c71ce3cf
Marcos Olguin <marcosaolguin@gmail.com>
2022-10-04 14:07:23 -0300

    commit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,14 @@
 import styles from "../styles/Footer.module.css";
 import Image from "next/image";
 import linkedinIcon from "../public/linkedin-icon.svg";
+import githubIcon from "../public/icon-github.svg";
 import gmail from "../public/google-gmail.svg";
 
 function Footer() {
   return (
     <div className={styles.container}>
       <div>Designed & Built by Marcos Olguin</div>
+
       <div>
         <a
           target="_blank"
@@ -18,7 +20,17 @@ function Footer() {
           <span>Linkedin</span>
         </a>
       </div>
-
+      <div>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/MarcosOlguin"
+          className={styles.iconContainer}
+        >
+          <Image src={githubIcon} width={25} height={20} alt={"img"} />
+          <span>GitHub</span>
+        </a>
+      </div>
       <div>
         <a className={styles.iconContainer}>
           <Image src={gmail} width={20} height={20} alt={"img"} />
